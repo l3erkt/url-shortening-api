eb02a3cad82307719b8ee9d42146736bcee964b9
Bereket Zeggai <bereketzeggai52@gmail.com>
2023-06-09 00:50:37 -0400

    - more responsive - added nav to mobile - created error msg for on no link

--- app.js
+++ app.js
@@ -2,18 +2,27 @@
 // 2. create a HTML injection to append towards the page
 // 3. implement html injection towards linkArr to display multiple links
 // 4. grab obj's from linkArr to display
-const infoContainer = document.querySelector('.info-container');
+const formContainer = document.querySelector(".link-form");
+const infoContainer = document.querySelector(".info-container");
 const shortLinkBtn = document.getElementById("link-btn");
 const linkInput = document.getElementById("url");
 const linkArr = [];
 
 storeLinks = () => {
-  const obj = {
-    orginalLink: linkInput.value,
-    newLink: "Working on it !!",
-  };
+  const span = document.createElement("span");
+  span.classList.add("error-msg");
+  span.innerText = "Please add a link";
 
-  linkArr.push(obj);
+  if (linkInput.value === "") {
+    linkInput.classList.add("error-bar");
+    formContainer.append(span);
+  } else {
+    const obj = {
+      orginalLink: linkInput.value,
+      newLink: "Working on it !!",
+    };
+    linkArr.push(obj);
+  }
 };
 
 injectHTML = () => {
@@ -34,15 +43,34 @@ injectHTML = () => {
   div.appendChild(div2);
   div2.appendChild(p2);
   div2.appendChild(button);
-  button.innerText = 'Copy';
-
-  console.log(linkArr);
+  button.innerText = "Copy";
 
   for (let i = 0; i < linkArr.length; i++) {
     p.innerText = linkArr[i].orginalLink;
     p2.innerText = linkArr[i].newLink;
-    infoContainer.prepend(div)
+    infoContainer.prepend(div);
   }
 };
 
 shortLinkBtn.addEventListener("click", () => injectHTML());
+
+// 1. burger btn
+const burgerBtn = document.getElementById("burger");
+const navContainer = document.querySelector(".nav-links");
+const blackBkg = document.querySelector('.black-bkg');
+
+dynamicNav = () => {
+  if (!navContainer.classList.contains("d-show")) {
+    navContainer.classList.add("d-show");
+    blackBkg.style.display = 'block'
+  } 
+};
+
+bkg = () => {
+  blackBkg.style.display = '';
+  navContainer.classList.remove("d-show");
+  
+}
+
+blackBkg.addEventListener('click', ()  => bkg())
+burgerBtn.addEventListener("click", () => dynamicNav());
